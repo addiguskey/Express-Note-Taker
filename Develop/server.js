d4865d206c2f4e40bbb6f64fbcb1d2489dcfd2ad
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-06-07 20:45:29 -0600

    first commit but everything is working

---
+++ Develop/server.js
@@ -0,0 +1,145 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const uuid = require("./helpers/uuid");
+
+//parsedNotes defined on line 105 in funciton pdateDb
+let parsedNotes;
+// newNote defined on line 67
+let newNote;
+//noteId defined on line 46
+let noteId;
+
+const PORT = 3001;
+const app = express();
+
+//MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//GET * should return index.html
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+
+//GET "/notes" should return notes.html
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+//When a new note is saved, this GET request appends the new note on the left side bar
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      consolg.error(err);
+    } else {
+      res.status(200).json(JSON.parse(data));
+    }
+  });
+});
+
+// GET  a single note
+app.get("/api/notes/:id", (req, res) => {
+  if (req.params.id) {
+    console.info(`${req.method} request received to get a single note`);
+    noteId = req.params.id;
+    let notes = require("./db/db.json");
+
+    for (let i = 0; i < notes.length; i++) {
+      const currentNote = notes[i];
+      if (currentNote.id === noteId) {
+        res.status(200).json(currentNote);
+        return;
+      }
+    }
+    res.status(404).send("Note not founud");
+  } else {
+    res.status(400).send("Note ID not provided");
+  }
+});
+
+//POST for new note with a random id, adding it to db.json
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+
+  const { title, text } = req.body;
+
+  if (title && text) {
+    newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+    updateDb();
+    const response = {
+      status: "sucess",
+      body: newNote,
+    };
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in posting note");
+  }
+});
+
+//POST request for a single review
+app.post("/api/notes/:id", (req, res) => {
+  if (req.body && req.params.id) {
+    console.info(`${req.method} request received to post a new note`);
+    let notes = require("./db/db.json");
+    const noteId = req.params.id;
+    for (let i = 0; i < notes.length; i++) {
+      const currentNote = notes[i];
+      if (currentNote.id === noteId) {
+        return;
+      }
+    }
+    res.status(404).json("Note ID not found");
+  }
+});
+
+//DELETE
+app.delete("/api/notes/:id", (req, res) => {
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    let noteId = req.params.id;
+    let updatedNotes = JSON.parse(data).filter((note) => {
+      console.log("note.id", note.id);
+      console.log("noteId", noteId);
+      return note.id !== noteId;
+    });
+    let notes = require("./db/db.json");
+    notes = updatedNotes;
+    const stringifyNote = JSON.stringify(updatedNotes);
+    fs.writeFile("./db/db.json", stringifyNote, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("note successfully deleted from db.json");
+      }
+    });
+    res.json(stringifyNote);
+  });
+});
+
+//function to update db
+function updateDb() {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      consolg.error(err);
+    } else {
+      parsedNotes = JSON.parse(data);
+      parsedNotes.push(newNote);
+
+      fs.writeFile("./db/db.json", JSON.stringify(parsedNotes), (wirteErr) =>
+        wirteErr
+          ? console.error(writeErr)
+          : console.info("Successfully updated notes!")
+      );
+    }
+  });
+}
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # Express-Note-Taker
+
+# User Story
+
+AS A small business owner
+I WANT to be able to write and save notes
+SO THAT I can organize my thoughts and keep track of tasks I need to complete
+
+# Table of Contents
+
+- [Installation](#installation)
+- [Preview](#preview)
+- [Usage](#usage)
+- [Badges](#badges)
+- [Questions](#questions)
+
+# Installation
+
+1. Clone this SSH Key link: "git@github.com:addiguskey/Express-Note-Taker.git" on your Terminal (Mac) or gitBash(Windows).
+
+2. This app uses [Express](https://www.npmjs.com/package/express). You can simply install these by running "npm i" on your Integrated Terminal in VS-Code
+
+3. Step by step guide video on how to use this app after cloning, is attached below in [Usage](#usage)
+
+# Preview
+
+# Usage
+
+GIVEN a note-taking application
+
+- WHEN I open the Note Taker,
+  THEN I am presented with a landing page with a link to a notes page
+- WHEN I click on the link to the notes page,
+  THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+- WHEN I enter a new note title and the note’s text,
+  THEN a Save icon appears in the navigation at the top of the page
+- WHEN I click on the Save icon,
+  THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+- WHEN I click on an existing note in the list in the left-hand column,
+  THEN that note appears in the right-hand column
+- WHEN I click on the Write icon in the navigation at the top of the page,
+  THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+- WHEN I click on the trashcan icon,
+  THEN the saved note gets deleted and is removed from the left-hand column where the existing notes are displayed
+
+# Badges
+
+![JS](https://img.shields.io/static/v1?label=JavaScript&message=66.6%&color=yellow) ![HTML](https://img.shields.io/static/v1?label=HTML&message=33.4%&color=red)
+
+# Questions
+
+For any questions, you can either reach out to me on GitHub or via e-mail
+
+- Username: @addiguskey
+- GitHub Link: https://github.com/addiguskey
+- E-mail: addisonguskey@gmail.com
