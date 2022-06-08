6c2e82100be108c40faea6a3851377aa6dc51372
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-06-08 09:43:32 -0600

    app.get update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //GET * should return index.html
-app.get("*", (req, res) =>
+app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
